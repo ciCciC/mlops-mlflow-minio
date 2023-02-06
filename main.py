685e4608aface636b4caebd674c6874062a6e8e9
koraypoyraz <koraypoyraz1@gmail.com>
2023-02-06 10:39:31 +0100

    l

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet
-from sklearn.datasets import load_wine
-
-import mlflow
 import mlflow.sklearn
 
 
